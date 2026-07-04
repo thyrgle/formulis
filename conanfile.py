0f1033ee8775069fa7e7bd726ca60e59574775ba
Christopher Sumnicht <csumnicht@berkeley.edu>
2026-07-03 21:39:06 -0700

    Fixed some test stuff.

--- conanfile.py
+++ conanfile.py
@@ -9,7 +9,7 @@ class Recipe(ConanFile):
         self.folders.generators = "conan"
 
     def requirements(self):
-        self.requires("fmt/11.0.2")
+        pass
 
     def build_requirements(self):
         self.test_requires("catch2/3.7.1")
