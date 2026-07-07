5f803935429775faae57aebea4967a4ad1906f00
Christopher Sumnicht <csumnicht@berkeley.edu>
2026-07-07 12:24:48 -0700

    Put operator overloading after class/type decls.

--- include/formulis/formulis.hpp
+++ include/formulis/formulis.hpp
@@ -229,6 +229,148 @@ public:
 };
 
 /**
+ * A "stmt" is either a term or formula. (Note: Sister library `forcamla` does
+ * *not* make this distinction.)
+ */
+template<typename T>
+using stmt =
+    std::variant<std::shared_ptr<term<T>>, std::shared_ptr<formula<T>>>;
+
+/**
+ * Keep track of a unary operation applied to some inner formula (the "rhs").
+ */
+template<typename T>
+struct unary_expr
+{
+  stmt<T> rhs;
+  std::function<T(T)> op;
+};
+
+/**
+ * Keep track of a binary operation applied to two (sub-)formula (the lhs and
+ * rhs).
+ */
+template<typename T>
+struct bin_expr
+{
+  stmt<T> lhs;
+  stmt<T> rhs;
+  std::function<T(T, T)> op;
+};
+
+/**
+ * `term` objects and `formula` objects are two of the fundamental types.
+ * `formula` objects are `term` objects combined with operations to represent
+ * a mathematical formula. They always evaluate to the most up-to-date value of
+ * each `term` they use in their expression.
+ */
+template<typename T>
+class formula
+{
+  bool m_needs_update;
+  std::variant<unary_expr<T>, bin_expr<T>> m_expr;
+  T m_cached_val;
+  std::vector<std::shared_ptr<formula<T>>> m_parents;
+  std::vector<std::function<void(T, T)>> m_on_change;
+
+public:
+  explicit formula(unary_expr<T> expr)
+      : m_needs_update(true)
+      , m_expr(expr)
+      , m_cached_val(eval())
+      , m_parents({})
+      , m_on_change({})
+  {
+  }
+
+  explicit formula(bin_expr<T> expr)
+      : m_needs_update(true)
+      , m_expr(expr)
+      , m_cached_val(eval())
+      , m_parents({})
+      , m_on_change({})
+  {
+  }
+
+  /**
+   * Indicate the formula's cached value is out of date and an update is needed
+   */
+  auto set_needs_update() -> void { m_needs_update = true; }
+
+  /**
+   * Get the current value of the formula.
+   */
+  auto eval() const -> T
+  {
+    if (!m_needs_update) {
+      return m_cached_val;
+    }
+    return std::visit(
+        overloaded {[](unary_expr<T> expression) -> T
+                    {
+                      auto inner = std::visit(
+                          overloaded {[](std::shared_ptr<term<T>> val) -> T
+                                      { return val->unwrap(); },
+                                      [](std::shared_ptr<formula<T>> ast) -> T
+                                      {
+                                        return (ast->m_needs_update
+                                                    ? ast->eval()
+                                                    : ast->m_cached_val);
+                                      }},
+                          expression.rhs);
+                      return expression.op(inner);
+                    },
+                    [](bin_expr<T> operand) -> T
+                    {
+                      auto inner_lhs = std::visit(
+                          overloaded {[](std::shared_ptr<term<T>> val) -> T
+                                      { return val->unwrap(); },
+                                      [](std::shared_ptr<formula<T>> ast) -> T
+                                      { return ast->eval(); }},
+                          operand.lhs);
+                      auto inner_rhs = std::visit(
+                          overloaded {[](std::shared_ptr<term<T>> val) -> T
+                                      { return val->unwrap(); },
+                                      [](std::shared_ptr<formula<T>> ast) -> T
+                                      { return ast->eval(); }},
+                          operand.rhs);
+                      return operand.op(inner_lhs, inner_rhs);
+                    }},
+        m_expr);
+  }
+
+  /**
+   * Update the cached value of the formula and also perform function calls
+   * if the value was changed.
+   */
+  auto update() -> void
+  {
+    T old_val = m_cached_val;
+    m_cached_val = eval();
+    if (old_val != m_cached_val) {
+      for (const auto& func : m_on_change) {
+        func(old_val, m_cached_val);
+      }
+      m_needs_update = false;
+      for (auto& parent : m_parents) {
+        parent->update();
+      }
+    }
+  }
+
+  /**
+   * Execute a function when the value of this formula changes.
+   * @param func The function to execute. Takes the old and current value of
+   * the formula. (The user is free to do with the old and current values as
+   * they wish.)
+   */
+  auto on_change(std::function<void(T, T)> func) -> void
+  {
+    m_on_change.push_back(func);
+  }
+};
+
+/**
  * Addition of two terms.
  */
 template<typename T>
@@ -573,145 +715,3 @@ auto operator&&(std::shared_ptr<term<T>>& lhs, std::shared_ptr<formula<T>>& rhs)
   rhs->m_parents.push_back(form);
   return form;
 }
-
-/**
- * A "stmt" is either a term or formula. (Note: Sister library `forcamla` does
- * *not* make this distinction.)
- */
-template<typename T>
-using stmt =
-    std::variant<std::shared_ptr<term<T>>, std::shared_ptr<formula<T>>>;
-
-/**
- * Keep track of a unary operation applied to some inner formula (the "rhs").
- */
-template<typename T>
-struct unary_expr
-{
-  stmt<T> rhs;
-  std::function<T(T)> op;
-};
-
-/**
- * Keep track of a binary operation applied to two (sub-)formula (the lhs and
- * rhs).
- */
-template<typename T>
-struct bin_expr
-{
-  stmt<T> lhs;
-  stmt<T> rhs;
-  std::function<T(T, T)> op;
-};
-
-/**
- * `term` objects and `formula` objects are two of the fundamental types.
- * `formula` objects are `term` objects combined with operations to represent
- * a mathematical formula. They always evaluate to the most up-to-date value of
- * each `term` they use in their expression.
- */
-template<typename T>
-class formula
-{
-  bool m_needs_update;
-  std::variant<unary_expr<T>, bin_expr<T>> m_expr;
-  T m_cached_val;
-  std::vector<std::shared_ptr<formula<T>>> m_parents;
-  std::vector<std::function<void(T, T)>> m_on_change;
-
-public:
-  explicit formula(unary_expr<T> expr)
-      : m_needs_update(true)
-      , m_expr(expr)
-      , m_cached_val(eval())
-      , m_parents({})
-      , m_on_change({})
-  {
-  }
-
-  explicit formula(bin_expr<T> expr)
-      : m_needs_update(true)
-      , m_expr(expr)
-      , m_cached_val(eval())
-      , m_parents({})
-      , m_on_change({})
-  {
-  }
-
-  /**
-   * Indicate the formula's cached value is out of date and an update is needed
-   */
-  auto set_needs_update() -> void { m_needs_update = true; }
-
-  /**
-   * Get the current value of the formula.
-   */
-  auto eval() const -> T
-  {
-    if (!m_needs_update) {
-      return m_cached_val;
-    }
-    return std::visit(
-        overloaded {[](unary_expr<T> expression) -> T
-                    {
-                      auto inner = std::visit(
-                          overloaded {[](std::shared_ptr<term<T>> val) -> T
-                                      { return val->unwrap(); },
-                                      [](std::shared_ptr<formula<T>> ast) -> T
-                                      {
-                                        return (ast->m_needs_update
-                                                    ? ast->eval()
-                                                    : ast->m_cached_val);
-                                      }},
-                          expression.rhs);
-                      return expression.op(inner);
-                    },
-                    [](bin_expr<T> operand) -> T
-                    {
-                      auto inner_lhs = std::visit(
-                          overloaded {[](std::shared_ptr<term<T>> val) -> T
-                                      { return val->unwrap(); },
-                                      [](std::shared_ptr<formula<T>> ast) -> T
-                                      { return ast->eval(); }},
-                          operand.lhs);
-                      auto inner_rhs = std::visit(
-                          overloaded {[](std::shared_ptr<term<T>> val) -> T
-                                      { return val->unwrap(); },
-                                      [](std::shared_ptr<formula<T>> ast) -> T
-                                      { return ast->eval(); }},
-                          operand.rhs);
-                      return operand.op(inner_lhs, inner_rhs);
-                    }},
-        m_expr);
-  }
-
-  /**
-   * Update the cached value of the formula and also perform function calls
-   * if the value was changed.
-   */
-  auto update() -> void
-  {
-    T old_val = m_cached_val;
-    m_cached_val = eval();
-    if (old_val != m_cached_val) {
-      for (const auto& func : m_on_change) {
-        func(old_val, m_cached_val);
-      }
-      m_needs_update = false;
-      for (auto& parent : m_parents) {
-        parent->update();
-      }
-    }
-  }
-
-  /**
-   * Execute a function when the value of this formula changes.
-   * @param func The function to execute. Takes the old and current value of
-   * the formula. (The user is free to do with the old and current values as
-   * they wish.)
-   */
-  auto on_change(std::function<void(T, T)> func) -> void
-  {
-    m_on_change.push_back(func);
-  }
-};
